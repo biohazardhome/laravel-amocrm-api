1d7999d8b8b78d3d7da734d0b83b69ec04d9acee
biohazardhome <biohazardhome@gmail.com>
2023-03-01 14:39:18 +0300

    Added models

---
+++ app/Models/Lead.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lead extends Model
+{
+    use HasFactory;
+
+    protected 
+        $fillable = [
+            'name',
+            'lead_id',
+            'group_id',
+            'account_id',
+            'pipeline_id',
+            'status_id',
+            'company_id',
+            'price',
+        ],
+        $casts = [
+            'name' => 'integer',
+            'lead_id' => 'integer',
+            'group_id' => 'integer',
+            'account_id' => 'integer',
+            'pipeline_id' => 'integer',
+            'status_id' => 'integer',
+            'company_id' => 'integer',
+            'price' => 'integer',
+        ];
+
+    public static function findByLeadId($id) {
+        return Lead::whereLeadId($id)->first();
+    }
+
+    public function pipeline() {
+        return $this->hasOne(Pipeline::class, 'pipeline_id', 'pipeline_id');
+    }
+
+    public function account() {
+        return $this->hasOne(Account::class, 'account_id', 'account_id');
+    }
+
+}
